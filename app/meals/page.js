6092213112e028536c9be9a6597f0c96e46514af
Yu Hao Shao <yuhaoshao@Yus-MacBook-Air.local>
2025-01-14 17:20:22 -0800

    meta data added

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,9 +4,13 @@ import styles from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid'
 import { getMeals } from '@/lib/meals'
 
+export const metadata = {
+    title: 'All Meals',
+    description: 'Explore delicious recipes shared by our community members !',
+};
+
 async function Meals (){
     const meals = await getMeals()
-    console.log('meals', meals)
     return  <MealsGrid meals={meals} />
 }
 
